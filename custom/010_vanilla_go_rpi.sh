aade7e92122b9048d8c8a6b4c5ed6b46f601cb53
Dmitry Fedotov <dafedotov@gmail.com>
2022-09-18 17:45:07 +0300

    rpi variant

--- custom/010_vanilla_go_rpi.sh
+++ custom/010_vanilla_go_rpi.sh
@@ -1,7 +1,7 @@
 # install vanilla Go 1.18.6
 set -e
 echo "fetching go compiler"
-curl --location https://go.dev/dl/go1.18.6.linux-amd64.tar.gz -o /tmp/go.tar.gz
+curl --location https://go.dev/dl/go1.18.6.linux-armv6l.tar.gz -o /tmp/go.tar.gz
 echo "unpacking go compiler..."
 tar -xzf /tmp/go.tar.gz -C /usr/local
 echo "removing archive"
